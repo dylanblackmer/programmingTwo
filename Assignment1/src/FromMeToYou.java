392acf4c992c96c76cbac9c0cbf43e4026a5e5bc
dblack1765 <dblack1765@51-209L-SD29.rcs.local>
2019-03-20 14:08:30 -0400

    Adds the cheeky programs I made beginning in programming 2

---
+++ Assignment1/src/FromMeToYou.java
@@ -0,0 +1,23 @@
+
+public class FromMeToYou {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("From: Dylan Blackmer");
+		System.out.println("Address: Adams High School, Room 209");
+		System.out.println("Date: March 19, 2019");
+		System.out.println("");
+		System.out.println("To: Daniel Carruthers");
+		System.out.println("");
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+		System.out.println("");
+		System.out.println("~Break~");
+		System.out.println("");
+		
+		System.out.println("From: Dylan Blackmer\nAddress: Adams High School, Room 209\nDate: March 19, 2019\n\nTo: Daniel Carruthers\n\nMessage: Help! I'm trapped inside a computer!");
+		
+
+	}
+
+}
---
+++ Assignment1/src/TestProject.java
@@ -0,0 +1,14 @@
+//My name is Dylan Blackmer
+//Welcome to my code :D
+
+//imports
+
+//actual class
+public class TestProject {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); //prints "Hello World"
+		
+	}
+}
