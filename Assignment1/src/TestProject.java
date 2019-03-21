ffae731e9fcdede1c072ff9e42ee6b344a1611b1
dblack1765 <dblack1765@51-209L-SD29.rcs.local>
2019-03-21 14:24:18 -0400

    fooled around with casting variables to use for Addignment 2

--- Assignment1/src/TestProject.java
+++ Assignment1/src/TestProject.java
@@ -2,17 +2,31 @@
 //Welcome to my code :D
 
 //imports
+import java.util.Scanner;
 
 //actual class
 public class TestProject {
 	
 	public static void main(String[] args) {
 		
+		/*
 		System.out.println("Hello World"); //prints "Hello World"
 		
 		//I'm adding a comment to test (written from desktop)
 		
 		//I'm adding a comment to test (written from laptop)
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Input: ");
+		double num = input.nextDouble();
+		
+		System.out.println(num);
+		
+		int numInt = (int) num;
+		
+		System.out.println(numInt);
 		
 	}
 }
