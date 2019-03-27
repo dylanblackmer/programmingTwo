3208facce1c4e7619ca37979fb05f1a94ec96346
Dylan Blackmer <Dylan Blackmer@10.92.51.84>
2019-03-27 18:22:15 -0400

    fixed error involving % error, added comments over entire program

--- Asignment 3/src/ShowMeTheMoney.java
+++ Asignment 3/src/ShowMeTheMoney.java
@@ -1,5 +1,7 @@
 //Dylan Blackmer	ShowMeTheMoney
 
+//Could be used in a CoinStar machine or something that needs to take large amounts of small change and then return cash
+
 //imports
 import java.util.Scanner;
 
@@ -9,17 +11,17 @@ public class ShowMeTheMoney {
 		// TODO Auto-generated method stub
 
 	//input
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the amount of change in cents (¢): ");
-		int cents = input.nextInt();
-		input.close();
+		Scanner input = new Scanner(System.in); //opens scanner
+		System.out.println("Enter the amount of change in cents (¢): "); //prompts user to give a cent value
+		int cents = input.nextInt(); //stores given cent value
+		input.close(); //closes scanner
 		
 	//change sorter
 		int hundreds = cents / 10000; //divides cent amount by 10,000 (one hundred dollars in cents), set as integer so program won't take the decimal into account.
 		cents = cents % 10000; //takes the remainder after the hundreds have been taken out, resets original variable.
 		int tens = cents / 1000; //divides cent remainder by 1,000 (ten dollars in cents), similar to hundreds set as integer.
 		cents = cents % 1000; //once again takes remainder, this cycle repeats for all change catagories.
-		int ones = cents % 100;
+		int ones = cents / 100;
 		cents = cents % 100;
 		int quarters = cents / 25;
 		cents = cents % 25;
@@ -29,14 +31,15 @@ public class ShowMeTheMoney {
 		cents = cents % 5;
 		int pennies = cents / 1;
 		
-	//inital output
+	//initial output
+		System.out.println(""); //blank space
 		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("Your change will be the following: ");
+		System.out.println("Your change will be the following: "); //lets the user know what's coming lol
 		System.out.println("");
 		
 	//control flow and final outputs
+		/* The following if/else statements check to see if each type of money is in "plural mode" (0 or 2 and higher)
+		 or in "singular mode" (1). The program then decided whether to print the type of currency with, or without, an "s". */
 		if ((hundreds == 0) || (hundreds >= 2)) {
 			System.out.println(hundreds + "\tHundreds");
 		} else {
@@ -72,8 +75,6 @@ public class ShowMeTheMoney {
 		} else {
 			System.out.println(pennies + "\tPenny");
 		}
-		
-		
 		
 	}
 
