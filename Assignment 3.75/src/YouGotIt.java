e06b45ebdde284ca5347ad31018af575b307a9bd
dblack1765 <dblack1765@51-209L-SD29.rcs.local>
2019-04-12 13:48:59 -0400

    finished Assignment 3.75

--- Assignment 3.75/src/YouGotIt.java
+++ Assignment 3.75/src/YouGotIt.java
@@ -26,24 +26,24 @@ public class YouGotIt {
 		while ((num != guess) && (counter < 10)) { //loop continues so long as user guesses incorrectly or guess count goes too high
 			String guessPromt = JOptionPane.showInputDialog("Guess a number between 1 and 300."); //promts user for a guess
 			guess = Integer.parseInt(guessPromt); //stores user guess
-			System.out.println("Guess: " + guess);
+			System.out.println("Guess: " + guess); //displays guess in console for debug
 			counter += 1;
-			if (num == guess) {
+			if (num == guess) { //checks to see if user guessed correctly
 				JOptionPane.showMessageDialog(null, "Great guess! The number was " + num + ".\n\nGuess count: " + counter);
-			} else if ((num < guess) && (guess <= tenAbove)) {
+			} else if ((num < guess) && (guess <= tenAbove)) { //tests if user was within 10 high of the actual number
 				JOptionPane.showMessageDialog(null, "Ooooof, so close bud. Just a liiiiiiittle too high, Guess again.\n\nGuess count: " + counter);
-			} else if ((tenBelow <= guess) && (guess < num)) {
+			} else if ((tenBelow <= guess) && (guess < num)) { //tests if user was within 10 low of actual number
 				JOptionPane.showMessageDialog(null, "Ouch, that's gotta hurt. Just a tad too low. Guess again.\n\nGuess count: " + counter);
-			} else if ((guess > 300) || (guess < 1)){
+			} else if ((guess > 300) || (guess < 1)){ //tests to ensure guess is in range
 				JOptionPane.showMessageDialog(null, "Error: Input out of range. Guess again.");
-				counter -= 1;
-			} else if (guess > num) {
+				counter -= 1; //user is not penalized for guesses out of range
+			} else if (guess > num) { //checks if guess is high
 				JOptionPane.showMessageDialog(null, "Too high my guy. Guess again.\n\nGuess count: " + counter);
-			} else if (guess < num) {
+			} else if (guess < num) { //checks if guess is low
 				JOptionPane.showMessageDialog(null, "Too low man. Guess again.\n\nGuess count: " + counter);
 			}
-			System.out.println("Guess Count: " + counter);
-			if (counter >= 10) {
+			System.out.println("Guess Count: " + counter); //displays number of guesses for debug
+			if (counter >= 10) { //checks to ensure user hasen't gone over guess limit
 				JOptionPane.showMessageDialog(null, "You ran out of guesses before you guessed correctly... better luck next time!");
 			}
 			
