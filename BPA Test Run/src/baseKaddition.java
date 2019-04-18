b91ea94497ddb93aac0adc6dfdc7449765508099
Dylan Blackmer <Dylan Blackmer@10.0.0.75>
2019-04-18 12:29:49 -0400

    added variables

--- BPA Test Run/src/baseKaddition.java
+++ BPA Test Run/src/baseKaddition.java
@@ -6,8 +6,11 @@ public class baseKaddition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	//variable
-		
+	//variable initialize
+		int base = 10;
+		int num1 = 0;
+		int num2 = 0;
+		int sum = 0;
 		
 	}
 
