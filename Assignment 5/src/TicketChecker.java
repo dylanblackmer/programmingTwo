4cdacd17507a7755a6fc4c27321f421cee61f936
Dylan Blackmer <Dylan Blackmer@10.92.51.87>
2019-04-26 08:51:21 -0400

    finished project, don't plan on making changes

--- Assignment 5/src/TicketChecker.java
+++ Assignment 5/src/TicketChecker.java
@@ -1,21 +1,34 @@
-import javax.swing.JOptionPane;
-
 //Dylan Blackmer	TicketChecker
 
 //imports
+import javax.swing.JOptionPane;
+
+
 public class TicketChecker {
 
 	public TicketChecker() {
 		
 	}
 	
-	public int ticketCheck() {
-		String ticketString = JOptionPane.showInputDialog(null, "Please enter ticket number: ", "Enter Ticket", JOptionPane.QUESTION_MESSAGE);
-		int firstFive = Integer.parseInt(ticketString.substring(0, 6));
-		System.out.println(firstFive);
-		int lastOne = Integer.parseInt(ticketString.substring(6));
-		System.out.println(lastOne);
-		return 0;
+	public int ticketCheck() { 
+		String ticketString = JOptionPane.showInputDialog(null, "Please enter ticket number: ", "Enter Ticket", JOptionPane.QUESTION_MESSAGE); //prompts user for ticket number
+		if (ticketString.length() != 6) { //checks ticket number for being too short or too long
+			JOptionPane.showMessageDialog(null, "Error: Invalid number, please re-enter.", "Error", JOptionPane.ERROR_MESSAGE); //alerts user
+			ticketCheck(); //restarts process
+		}
+		int ticketNumber = Integer.parseInt(ticketString); //turns the ticket string into an int to return later
+		int firstFive = Integer.parseInt(ticketString.substring(0, 5)); //takes first 5 digits and adds to int
+		System.out.println(firstFive); //output for debug
+		int lastOne = Integer.parseInt(ticketString.substring(5)); //takes last digit and adds to int
+		System.out.println(lastOne); //output for debug
+		
+		int remainder = firstFive % 7; //takes the remainder of the first 5 digits after dividing by 7
+		if (remainder == lastOne) { //checks to make sure the remainder is equal to the last digit 
+			return ticketNumber; //returns ticket
+		} else {
+			return 0; //returns 0
+		}
+		
 		
 	}
 	
--- Assignment 5/src/TicketNumber.java
+++ Assignment 5/src/TicketNumber.java
@@ -9,9 +9,16 @@ public class TicketNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		TicketChecker ticketChecker = new TicketChecker();
+		TicketChecker ticketChecker = new TicketChecker(); //communicates with other class
 		
-		ticketChecker.ticketCheck();
+		int ticket = ticketChecker.ticketCheck(); //calls method from "TicketChecker" and stores returned value to "ticket" 
+		
+		if (ticket != 0) { //if the ticket didn't work, "TicketChecker" returns "0", so long as "ticket" doesn't equal zero, the operator will receive a success message, otherwise they will be asked to retry and sent back to the beginning.
+			JOptionPane.showMessageDialog(null, "Correct ticket number!\n\nTicket Number: \n" + ticket, "Success!", JOptionPane.PLAIN_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect ticket number, please re-enter.", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		
 		
 	}
 
