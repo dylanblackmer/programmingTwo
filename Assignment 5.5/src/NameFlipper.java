d7454e2ec070ecb095b157534ebce8fb0a588c06
Dylan Blackmer <Dylan Blackmer@10.92.51.71>
2019-04-30 13:32:05 -0400

    new assignment

---
+++ Assignment 5.5/src/NameFlipper.java
@@ -0,0 +1,10 @@
+
+public class NameFlipper {
+
+	public NameFlipper() {
+		
+	}
+	
+	
+	
+}
---
+++ Assignment 5.5/src/RedRum.java
@@ -0,0 +1,11 @@
+
+public class RedRum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		NameFlipper flip = new NameFlipper();
+		
+	}
+
+}
