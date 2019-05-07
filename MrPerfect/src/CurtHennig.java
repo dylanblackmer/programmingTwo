d980fc3efc05e3932bf54d8ebbe10c8bd83808af
dblack1765 <dblack1765@51-209L-SD29.rcs.local>
2019-05-07 14:23:22 -0400

    fuc

---
+++ MrPerfect/src/CurtHennig.java
@@ -0,0 +1,16 @@
+//Dylan Blackmer	CurtHennig
+
+//imports
+
+
+public class CurtHennig {
+
+	public static void main(String[] args) {
+		
+		LarryHennig theAxe = new LarryHennig();
+		theAxe.numberRunner();
+		
+		
+	}
+	
+}
---
+++ MrPerfect/src/LarryHennig.java
@@ -0,0 +1,27 @@
+//Dylan Blackmer	LarryHennig
+
+//imports
+
+
+public class LarryHennig {
+	
+	public LarryHennig() {
+		
+	}
+	
+	public void numberRunner() {
+		for (int i = 1; i <= 327000; i++) {
+			perfectTester(i);
+		}
+	}
+	
+	public void perfectTester(int num) {
+		for (int i = num - 1; i >= 1; i--) {
+			if ((num % i) == 0) {
+				
+			} else {
+				
+			}
+		}
+	}
+}
