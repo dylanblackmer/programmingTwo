2446fd3cd4b7271659e5abb16a7c256f6c322256
Dylan Blackmer <Dylan Blackmer@10.92.51.106>
2019-05-08 11:32:47 -0400

    initialized and finished

---
+++ Mr Perfect/src/CurtHennig.java
@@ -0,0 +1,16 @@
+//Dylan Blackmer	CurtHennig
+
+//imports
+
+
+public class CurtHennig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LarryHennig theAxe = new LarryHennig();
+		theAxe.numberRunner();
+		
+	}
+
+}
